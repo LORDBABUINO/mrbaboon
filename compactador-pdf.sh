7f9f56ff0d4f085eea436c1f55bc0e67860857c4
LORDBABUINO <jorge.x7@gmail.com>
2017-10-17 17:16:51 -0200

    bug no procedimento find do arquivo compactador-pdf.sh concertado

--- compactador-pdf.sh
+++ compactador-pdf.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 diretory=$1
-mkdir $diretory/../backup-files
+sudo mkdir $diretory/../backup-files
 
-sudo find $diretory -type f -iname *.pdf | while read -r file
+sudo find $diretory -type f -iname "*.pdf" | while read -r file
 do
   gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dBATCH -dQUIET -sOutputFile="$file.pdf" "$file"
 
